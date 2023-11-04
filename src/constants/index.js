2a6983acf5dc1e4af39f1a926922aeed3c68ad12
shinnkura <n.shinzo1010@icloud.com>
2023-11-04 16:56:55 +0900

    feat(Bottombar, LeftSidebar, RightSidebar, Topbar): Bottombar, LeftSidebar, RightSidebar, Topbarのコンポ

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/home.png",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/search.png",
+    route: "/search",
+    label: "Search",
+  },
+  {
+    imgURL: "/assets/heart.png",
+    route: "/activity",
+    label: "Activity",
+  },
+  {
+    imgURL: "/assets/create.png",
+    route: "/create-thread",
+    label: "Create Thread",
+  },
+  {
+    imgURL: "/assets/community.png",
+    route: "/communities",
+    label: "Communities",
+  },
+  {
+    imgURL: "/assets/user.png",
+    route: "/profile",
+    label: "Profile",
+  },
+];
+
+export const profileTabs = [
+  { value: "threads", label: "Threads", icon: "/assets/reply.png" },
+  { value: "replies", label: "Replies", icon: "/assets/members.png" },
+  { value: "tagged", label: "Tagged", icon: "/assets/tag.png" },
+];
+
+export const communityTabs = [
+  { value: "threads", label: "Threads", icon: "/assets/reply.png" },
+  { value: "members", label: "Members", icon: "/assets/members.png" },
+  { value: "requests", label: "Requests", icon: "/assets/request.png" },
+];

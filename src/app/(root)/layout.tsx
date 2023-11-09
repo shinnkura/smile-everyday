ede54d612c907866dcbd75bd6f28a555efb731b5
shinnkura <n.shinzo1010@icloud.com>
2023-11-09 12:14:12 +0900

    chore(RightSidebar.tsx): RightSidebar.tsxの削除

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,7 +28,6 @@ export default function RootLayout({
             <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
-            <RightSidebar />
           </main>
           <Bottombar />
         </body>

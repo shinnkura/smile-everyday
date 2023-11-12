ae04387114bb22ec75a9cba81311bfebcb7d4b19
shinnkura <n.shinzo1010@icloud.com>
2023-11-12 13:39:41 +0900

    style(globals.css): bottombar_containerのstyleを修正

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -186,6 +186,27 @@ const config: Config = {
         "gray-1": "#697C89",
         glassmorphism: "rgba(16, 16, 18, 0.60)",
       },
+      boxShadow: {
+        "count-badge": "0px 0px 6px 2px rgba(219, 188, 159, 0.30)",
+        "groups-sidebar": "-30px 0px 60px 0px rgba(28, 28, 31, 0.50)",
+      },
+      screens: {
+        xs: "400px",
+      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: 0 },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: 0 },
+      //   },
+      // },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],

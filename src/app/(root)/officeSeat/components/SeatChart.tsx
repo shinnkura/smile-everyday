c0b3f635ee3e48b51d3099cbe08235ff343faed3
shinnkura <n.shinzo1010@icloud.com>
2023-11-13 18:39:44 +0900

    style(SeatGrid.tsx): SeatGridコンポーネントを作成

--- src/app/(root)/officeSeat/components/SeatChart.tsx
+++ src/app/(root)/officeSeat/components/SeatChart.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import SeatGrid from "./SeatGrid";
 
-// SeatChart コンポーネント
 const SeatChart = () => {
   return (
     <div className="p-5">
@@ -9,126 +9,53 @@ const SeatChart = () => {
         <p className="text-center">[2023/08/17]</p>
       </div>
 
-      <div className="flex flex-wrap -mx-2">
+      <div className="flex flex-wrap -mx-2 text-center">
         <div className="w-1/4 px-2">
-          <div className="mb-4 p-2 bg-gray-200">ロッカー</div>
+          <div className="mb-6 p-2 bg-gray-200">ロッカー</div>
         </div>
-
         <div className="w-1/2 px-2">
-          <div className="mb-4 p-2 bg-gray-300">お菓子</div>
+          <div className="mb-6 p-2 bg-gray-300">お菓子</div>
         </div>
-
         <div className="w-1/4 px-2">
-          <div className="mb-4 p-2 bg-gray-200">入り口</div>
+          <div className="mb-6 p-2 bg-gray-200">入り口</div>
         </div>
       </div>
 
-      <div className="grid grid-cols-[4fr_4fr_1fr] gap-4">
-        <div className="flex flex-col gap-2">
-          <div className="bg-red-400 grid grid-rows-2 grid-cols-3 gap-2">
-            <div>左上 1</div>
-            <div>左上 2</div>
-            <div>左上 3</div>
-            <div>左上 4</div>
-            <div>左上 5</div>
-            <div>左上 6</div>
-          </div>
-          <div className="bg-red-500 grid grid-rows-2 grid-cols-3 gap-2">
-            <div>左下 1</div>
-            <div>左下 2</div>
-            <div>左下 3</div>
-            <div>左下 4</div>
-            <div>左下 5</div>
-            <div>左下 6</div>
-          </div>
+      <div className="grid grid-cols-[7fr_7fr_1fr] gap-4 text-center">
+        <div className="flex flex-col gap-2 mb-8">
+          <SeatGrid color="red" rows={2} cols={3} sectionName="左上" />
+          <SeatGrid color="red" rows={2} cols={3} sectionName="左下" />
         </div>
-        <div className="flex flex-col gap-2">
-          <div className="bg-green-400 grid grid-rows-2 grid-cols-4 gap-2">
-            <div>中央上 1</div>
-            <div>中央上 2</div>
-            <div>中央上 3</div>
-            <div>中央上 4</div>
-            <div>中央上 5</div>
-            <div>中央上 6</div>
-            <div>中央上 7</div>
-            <div>中央上 8</div>
-          </div>
-          <div className="bg-green-500 grid grid-rows-2 grid-cols-4 gap-2">
-            <div>中央下 1</div>
-            <div>中央下 2</div>
-            <div>中央下 3</div>
-            <div>中央下 4</div>
-            <div>中央下 5</div>
-            <div>中央下 6</div>
-            <div>中央下 7</div>
-            <div>中央下 8</div>
-          </div>
+        <div className="flex flex-col gap-2 mb-8">
+          <SeatGrid color="green" rows={2} cols={4} sectionName="中央上" />
+          <SeatGrid color="green" rows={2} cols={4} sectionName="中央下" />
         </div>
-        <div className="bg-blue grid grid-rows-2 grid-cols-1 gap-2">
-          <div>右 1</div>
-          <div>右 2</div>
+        <div className="bg-gray-200 grid grid-rows-2 grid-cols-1 gap-2 mb-5">
+          <div>水</div>
+          <div>備品</div>
         </div>
       </div>
 
-      <div className="grid grid-cols-5 gap-4">
-        <div className="bg-red-400">左 1x1</div>
-
+      <div className="grid grid-cols-[1fr_8fr_8fr_8fr_1fr] gap-4 text-center">
+        <div className="bg-gray-200">集中ブース</div>
         <div className="flex flex-col gap-2">
-          <div className="bg-red-500 grid grid-rows-3 grid-cols-2 gap-2">
-            <div>左中央上 1</div>
-            <div>左中央上 2</div>
-            <div>左中央上 3</div>
-            <div>左中央上 4</div>
-            <div>左中央上 5</div>
-            <div>左中央上 6</div>
-          </div>
-          <div className="bg-red-600 grid grid-rows-2 grid-cols-2 gap-2">
-            <div>左中央下 1</div>
-            <div>左中央下 2</div>
-            <div>左中央下 3</div>
-            <div>左中央下 4</div>
-          </div>
+          <SeatGrid color="red" rows={3} cols={2} sectionName="左中央上" />
+          <div className="bg-red-400 py-2">左中央中央 1x1</div>
+          <SeatGrid color="red" rows={2} cols={2} sectionName="左中央下" />
         </div>
-
         <div className="flex flex-col gap-2">
-          <div className="bg-green-400">中央上 1x1</div>
-          <div className="bg-green-500 grid grid-rows-3 grid-cols-2 gap-2">
-            <div>中央上中央 1</div>
-            <div>中央上中央 2</div>
-            <div>中央上中央 3</div>
-            <div>中央上中央 4</div>
-            <div>中央上中央 5</div>
-            <div>中央上中央 6</div>
-          </div>
-          <div className="bg-green-600">中央下中央 1x1</div>
-          <div className="bg-green-700 grid grid-rows-2 grid-cols-2 gap-2">
-            <div>中央下 1</div>
-            <div>中央下 2</div>
-            <div>中央下 3</div>
-            <div>中央下 4</div>
-          </div>
+          <div className="bg-green-400 py-2">中央上 1x1</div>
+          <SeatGrid color="green" rows={3} cols={2} sectionName="中央上中央" />
+          <div className="bg-green-600 py-2">中央下中央 1x1</div>
+          <SeatGrid color="green" rows={2} cols={2} sectionName="中央下" />
         </div>
-
         <div className="flex flex-col gap-2">
-          <div className="bg-blue grid grid-rows-4 grid-cols-2 gap-2">
-            <div>右中央上 1</div>
-            <div>右中央上 2</div>
-            <div>右中央上 3</div>
-            <div>右中央上 4</div>
-            <div>右中央上 5</div>
-            <div>右中央上 6</div>
-            <div>右中央上 7</div>
-            <div>右中央上 8</div>
-          </div>
-          <div className="bg-blue grid grid-rows-1 grid-cols-2 gap-2">
-            <div>右中央下 1</div>
-            <div>右中央下 2</div>
-          </div>
+          <SeatGrid color="red" rows={4} cols={2} sectionName="右中央上" />
+          <SeatGrid color="red" rows={1} cols={2} sectionName="右中央下" />
         </div>
-
         <div className="flex flex-col gap-2">
-          <div className="bg-blue">右上 1x1</div>
-          <div className="bg-blue">右下 1x1</div>
+          <div className="bg-gray-200">集中ブース</div>
+          <div className="bg-gray-200">集中ブース</div>
         </div>
       </div>
 
---
+++ src/app/(root)/officeSeat/components/SeatGrid.tsx
@@ -0,0 +1,29 @@
+interface SectionProps {
+  color: "red" | "green" | "blue" | "gray";
+  rows: number;
+  cols: number;
+  sectionName: string;
+}
+
+const SeatGrid: React.FC<SectionProps> = ({
+  color,
+  rows,
+  cols,
+  sectionName,
+}) => {
+  const seats = Array.from(
+    { length: rows * cols },
+    (_, i) => `${sectionName} ${i + 1}`
+  );
+  return (
+    <div className={` grid grid-rows-${rows} grid-cols-${cols} gap-2 py-4`}>
+      {seats.map((seat) => (
+        <div className={`bg-${color}-400 py-2`} key={seat}>
+          {seat}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SeatGrid;

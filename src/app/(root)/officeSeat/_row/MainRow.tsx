200cc16ab53009ac9879a32210a6f1e992e18717
shinnkura <n.shinzo1010@icloud.com>
2023-11-21 12:19:19 +0900

    style(officeSeat): 余白を追加

--- src/app/(root)/officeSeat/_row/MainRow.tsx
+++ src/app/(root)/officeSeat/_row/MainRow.tsx
@@ -1,14 +1,14 @@
 import SeatGrid from "./components/SeatGrid";
 
-const MainArea = () => (
+const MainRow = () => (
   <div className="grid grid-cols-[6fr_7fr_1fr] gap-4 text-center">
     <div className="flex flex-col gap-6 mb-8">
       <SeatGrid color="red" rows={2} cols={3} gridId={"1"} />
       <SeatGrid color="red" rows={2} cols={3} gridId={"2"} />
     </div>
-    <div className="flex flex-col gap-2 mb-8">
-      <SeatGrid color="sky" rows={2} cols={4} gridId={"3"} />
-      <SeatGrid color="yellow" rows={2} cols={4} gridId={"4"} />
+    <div className="flex flex-col gap-6 mb-8">
+      <SeatGrid color="gray" rows={2} cols={4} gridId={"3"} />
+      <SeatGrid color="gray" rows={2} cols={4} gridId={"4"} />
     </div>
     <div className="bg-gray-200 grid grid-rows-2 grid-cols-1 gap-2 mb-5">
       <div>水</div>
@@ -17,4 +17,4 @@ const MainArea = () => (
   </div>
 );
 
-export default MainArea;
+export default MainRow;
